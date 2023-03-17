b449c6527cda94d839ab072fd6704aa4f64e556b
spicycurrey <spiceycurrey@gmail.com>
2023-03-17 14:10:14 -0700

    Modified magic numbers for EnemySurfaceManager

--- Assets/Scripts/Scanner/ScannerSurfaces/EnemySurfaceManager.cs
+++ Assets/Scripts/Scanner/ScannerSurfaces/EnemySurfaceManager.cs
@@ -6,8 +6,8 @@ namespace Detection
     public class EnemySurfaceManager : MonoBehaviour
     {
         public int hitCount = 0;
-        public int maxHit { private set; get; } = 200;
-        public int hitThreshold { private set; get; } = 40;
+        public int maxHit { private set; get; } = 250;
+        public int hitThreshold { private set; get; } = 25;
         public DissolveController dissolveController { private set; get; }
         public bool runningReduceHitCount { private set; get; } = false;
         private WaitForSeconds reduceHitInterval = new WaitForSeconds(0.1f);
@@ -27,7 +27,7 @@ namespace Detection
             runningReduceHitCount = true;
             while (hitCount > 0)
             {
-                hitCount -= 25;
+                hitCount -= 10;
                 yield return reduceHitInterval;
             }
             dissolveController.Disappear();
