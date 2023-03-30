933161c5cebaa064787646f96900792a3cd84705
cambatch <cameron.batchelor22@gmail.com>
2023-03-29 19:26:11 -0700

    Added time based scoring and modified kill scoring

--- Assets/Scripts/Misc/ScoreManager.cs
+++ Assets/Scripts/Misc/ScoreManager.cs
@@ -8,17 +8,23 @@ namespace Detection
     {
         public static ScoreManager instance;
 
-        [SerializeField] private float killPoints;                // How much kills are worth
-        [SerializeField] private float specialPoints;             // How much special kills are worth
-        [SerializeField] private float comboTimeFrame = 3.0f;     // Combo timer max timeout time
-        public float difficultyModifier = 1.0f;
-        private float score = 0;
-        private float levelStartTime = 0f;
-        private int currentComboCount = 0;              // number of kills in a combo
-        private float currentComboScore = 0f;           // the combo the player is in
+        [Header("Point values")]
+        [SerializeField] private int killPoints = 1000;             // How much kills are worth
+        [SerializeField] private int specialPoints = 2000;          // How much special kills are worth
+        [SerializeField] private int maxLevelTimePoints = 20000;    // Amount of points to give player with fastest speed
+        [Header("Time values (seconds)")]
+        [SerializeField] private float comboTimeFrame = 3.0f;       // Combo timer timeout(seconds)
+        [SerializeField] private float minTimeInLevel = 1f;         // The minimum amount of time(seconds) it takes to complete a level
+        [SerializeField] private float maxTimeInLevel = 300f;       // The maximum amount of time(seconds) it takes to complete a level
+
+        private float difficultyModifier = 1.0f;
+        private int currentComboCount = 0;                          // Number of kills in a combo
+        private float runningComboTimer = 0f;                       // Running combo timer
+        private float score = 0f;                                   // Final score at the end of the level
+        private float currentComboScore = 0f;                       // Score of the combo player is in (reset on combo timer)
+        private float killScore = 0f;                               // Running score for kills (reset on level fail)
+        private float timeScore = 0f;                               // Score based on the time taken to beat level
         private bool comboTimerRunning = false;
-        private float runningComboTimer = 0f;           // running combo timer
-        private float currentLevelScore = 0f;           // current level score (reset on level fail)
 
         private void Awake()
         {
@@ -67,8 +73,10 @@ namespace Detection
                     Reset();
                     break;
                 case GameState.LEVELENDED:
-                    CalculateTimeScore();
-                    //SaveScore();
+                    CalculateTimeScore(Time.timeSinceLevelLoad);
+                    CalculateFinalScore();
+                    break;
+                default:
                     break;
             }
         }
@@ -93,27 +101,43 @@ namespace Detection
         private void CalculateComboScore()
         {
             float comboMultiplier = 1.0f + (currentComboCount * 0.1f);
-            currentLevelScore += currentComboScore + (currentComboScore * comboMultiplier) * difficultyModifier;
-        
-            //Debug.Log(currentLevelScore);
+
+            if (currentComboCount >= 1)
+                killScore += currentComboScore + currentComboScore * comboMultiplier * difficultyModifier;
+            else
+                killScore += currentComboScore * comboMultiplier * difficultyModifier;
         }
 
-        private void CalculateTimeScore()
+        private void CalculateTimeScore(float secondsInLevel)
         {
-            float totalLevelTime = Time.time - levelStartTime;
+            if (secondsInLevel <= minTimeInLevel)
+            {
+                timeScore = maxLevelTimePoints;
+            }
+            else if (secondsInLevel >= maxTimeInLevel)
+            {
+                timeScore = 0;
+            }
+            else
+            {
+                timeScore = maxLevelTimePoints * Mathf.Pow(maxTimeInLevel - secondsInLevel, 2) / Mathf.Pow(maxTimeInLevel, 2);
+            }
+        }
 
-            // score based on time...
+        private void CalculateFinalScore()
+        {
+            score = killScore + timeScore;
+            Debug.Log("Level Score: " + (int)score);
         }
 
         private void Reset()
         {
             comboTimerRunning = false;
-            currentComboScore = 0f;
-            currentLevelScore = 0f;
             currentComboCount = 0;
-
-            // Get starting time for timer
-            levelStartTime = Time.time;
+            currentComboScore = 0f;
+            killScore = 0f;
+            timeScore = 0f;
+            score = 0f;
         }
 
         private void SaveScore()
